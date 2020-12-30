bb281aed68116ad5c81fddfeea2010876e684e16
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-30 08:27:28 -0700

    sound

--- t2s.py
+++ t2s.py
@@ -1,9 +1,28 @@
 # This project is about text to speech conversion
 
 import os
-import gTTS
+from gtts import gTTS
 # Importing libraries required for this project
 # Import gTTS will help in converting a string to audio
 # Import os is for comversion of an audio file to playable
 # Import os is also for file handling
-
+ 
+  
+# The text that you want to convert to audio 
+mytext = 'Welcome to geeksforgeeks!'
+  
+# Language in which you want to convert 
+language = 'en'
+  
+# Passing the text and language to the engine,  
+# here we have marked slow=False. Which tells  
+# the module that the converted audio should  
+# have a high speed 
+myobj = gTTS(text=mytext, lang=language, slow=False) 
+  
+# Saving the converted audio in a mp3 file named 
+# welcome  
+myobj.save("welcome.mp3") 
+  
+# Playing the converted file 
+# os.system('welcome.mp3')
