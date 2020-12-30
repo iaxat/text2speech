96853eb006e04d2aa38ac14d868f79ade812e487
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-30 08:13:40 -0700

    details about library

--- t2s.py
+++ t2s.py
@@ -4,4 +4,6 @@ import os
 import gTTS
 # Importing libraries required for this project
 # Import gTTS will help in converting a string to audio
-# Import os is for comversion of 
+# Import os is for comversion of an audio file to playable
+# Import os is also for file handling
+
