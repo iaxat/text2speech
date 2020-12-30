a8423f57a160cd1fe3971b52912a5a5e65f43a11
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-30 10:15:41 -0700

    function

--- t2s.py
+++ t2s.py
@@ -7,23 +7,25 @@ from gtts import gTTS
 # Import os is for comversion of an audio file to playable
 # Import os is also for file handling
  
+def t2s():
+    # The text that you want to convert to audio 
+    my_file = open('file','r')
+    
+    # Language in which you want to convert 
+    language = 'en'
+    
+    
+    # Passing the text and language to the engine,  
+    # here we have marked slow=False. Which tells  
+    # the module that the converted audio should  
+    # have a high speed 
+    myobj = gTTS(text=mytext, lang=language, slow=False) 
+    
+    # Saving the converted audio in a mp3 file named 
+    # welcome  
+    myobj.save("welcome.mp3") 
   
-# The text that you want to convert to audio 
-mytext = 'Welcome to geeksforgeeks!'
-  
-# Language in which you want to convert 
-language = 'en'
-  
-# Passing the text and language to the engine,  
-# here we have marked slow=False. Which tells  
-# the module that the converted audio should  
-# have a high speed 
-myobj = gTTS(text=mytext, lang=language, slow=False) 
-  
-# Saving the converted audio in a mp3 file named 
-# welcome  
-myobj.save("welcome.mp3") 
-  
-# Playing the converted file 
-# os.system('welcome.mp3')
+    # Playing the converted file 
 
+
+t2s()
