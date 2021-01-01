1eba12a77453c9a749d00e3c0492619628c5ea2d
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-01 12:51:21 -0700

    modify

--- t2s.py
+++ t2s.py
@@ -15,12 +15,11 @@ def t2s():
     # Language in which you want to convert 
     language = 'en'
     
-    
     # Passing the text and language to the engine,  
     # here we have marked slow=False. Which tells  
     # the module that the converted audio should  
     # have a high speed 
-    myobj = gTTS(text=mytext, lang=language, slow=False) 
+    myobj = gTTS(text=my_file, lang=language, slow=False) 
     
     # Saving the converted audio in a mp3 file named 
     # welcome  
